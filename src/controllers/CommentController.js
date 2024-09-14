87c8793853ea077a1a08243efaffa07b996abba4
Sarthak vyas <142726436+Sarthak-vyas183@users.noreply.github.com>
2024-09-15 00:39:34 +0530

    comment feature implemented : testing Success

--- src/controllers/CommentController.js
+++ src/controllers/CommentController.js
@@ -1,9 +1,8 @@
-import mongoose, { isValidObjectId } from "mongoose"
-import {ApiError} from "../utils/Apierror.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
-import commentModel from "../models/commentModel.js"
-
+import mongoose, { isValidObjectId } from "mongoose";
+import { ApiError } from "../utils/Apierror.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import commentModel from "../models/commentModel.js";
 
 const getVideoComments = asyncHandler(async (req, res) => {
   try {
@@ -34,11 +33,11 @@ const getVideoComments = asyncHandler(async (req, res) => {
       },
       {
         $project: {
-          "ownerDetail.password": 0,       // Exclude password
-          "ownerDetail.watchHistory": 0,   // Exclude watchHistory
-          "ownerDetail.refreshToken": 0,   // Exclude refreshToken
+          "ownerDetail.password": 0, // Exclude password
+          "ownerDetail.watchHistory": 0, // Exclude watchHistory
+          "ownerDetail.refreshToken": 0, // Exclude refreshToken
         },
-      }
+      },
     ]);
 
     // Check if comments exist
@@ -47,72 +46,70 @@ const getVideoComments = asyncHandler(async (req, res) => {
     }
 
     // Return the fetched comments
-    return res.status(200).json(new ApiResponse(200, comments, "Comments fetched successfully"));
+    return res
+      .status(200)
+      .json(new ApiResponse(200, comments, "Comments fetched successfully"));
   } catch (error) {
     res.status(500).send(`Internal server error: ${error}`);
   }
 });
 
-
-
 // Add comment on a Particular video
 const addComment = asyncHandler(async (req, res) => {
-    try {
-       const comment = await commentModel.create({
-          content : req.body.content,
-          video : req.params.videoId,
-          owner : req.user?._id
-       });
-       if(!comment) throw new ApiError(405,"failed to comment on Video");
-       return res.status(200)
-                 .json(new ApiResponse(200, comment, "comment Success"));
-
-    } catch (error) {
-        res.send(`Internal server Error : ${error}`)
-    }
-})
+  try {
+    const comment = await commentModel.create({
+      content: req.body.content,
+      video: req.params.videoId,
+      owner: req.user?._id,
+    });
+    if (!comment) throw new ApiError(405, "failed to comment on Video");
+    return res
+      .status(200)
+      .json(new ApiResponse(200, comment, "comment Success"));
+  } catch (error) {
+    res.send(`Internal server Error : ${error}`);
+  }
+});
 
 // update comment
 const updateComment = asyncHandler(async (req, res) => {
-   try {
-         const {content} = req.body;
-         if(content?.trim() === "") {
-            throw new ApiError(404, "content is content is Require to Update")
-         }
-      const comment = await commentModel.findOneAndUpdate({
-             _id : req.params.commentId,
-             owner : req.user?._id
-      },{
-          content,
-      })
-      if(!comment) throw new ApiError(404, "failed to update comment");
-      return res.status(200)
-      .json(new ApiResponse(200, comment, "comment updated"));
-
-   } catch (error) {
-     res.status(500).send(`Internal server Error : ${error}`)
-   }
-})
-
-// delete comment 
-const deleteComment = asyncHandler(async (req, res) => {
-    try {
-     const deleted_comment = await commentModel.findOneAndDelete({
-            _id : req.params.commentId,
-            owner : req.user?._id
-     })
-     if(!deleted_comment) throw new ApiError(404, "failed to delete comment");
-     return res.status(200)
-     .json(new ApiResponse(200, deleted_comment, "comment updated"));
-
-  } catch (error) {
-    res.status(500).send(`Internal server Error : ${error}`)
-  }
-})
-
-export {
-    getVideoComments, 
-    addComment, 
-    updateComment,
-     deleteComment
+  try {
+    const { content } = req.body;
+    if (content?.trim() === "") {
+      throw new ApiError(404, "content is content is Require to Update");
     }
+    const comment = await commentModel.findOneAndUpdate(
+      {
+        _id: req.params.commentId,
+        owner: req.user?._id,
+      },
+      {
+        content,
+      }
+    );
+    if (!comment) throw new ApiError(404, "failed to update comment");
+    return res
+      .status(200)
+      .json(new ApiResponse(200, comment, "comment updated"));
+  } catch (error) {
+    res.status(500).send(`Internal server Error : ${error}`);
+  }
+});
+
+// delete comment
+const deleteComment = asyncHandler(async (req, res) => {
+  try {
+    const deleted_comment = await commentModel.findOneAndDelete({
+      _id: req.params.commentId,
+      owner: req.user?._id,
+    });
+    if (!deleted_comment) throw new ApiError(404, "failed to delete comment");
+    return res
+      .status(200)
+      .json(new ApiResponse(200, deleted_comment, "comment updated"));
+  } catch (error) {
+    res.status(500).send(`Internal server Error : ${error}`);
+  }
+});
+
+export { getVideoComments, addComment, updateComment, deleteComment };
